ad1d682888a594d268a949f995e68880c92a01c4
Alexey Yurchenko <homonoidian@yandex.ru>
2022-06-29 20:15:08 +0300

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # This file feels sketchy as fuck.
-git clone https://github.com/termbox/termbox2.git src/termbox/lib/termbox2
-cd src/termbox/lib/termbox2
+git clone https://github.com/termbox/termbox2.git src/termbox2/lib/termbox2
+cd src/termbox2/lib/termbox2
 make libtermbox.so
